36ab8f354a43bd738a8037fe6e22b9e9c860360b
hsb-tonmoy <hsb.tonmoy@gmail.com>
2021-04-18 04:10:36 -0400

    Math rendering properly

--- src/pages/questions/[slug]/index.svelte
+++ src/pages/questions/[slug]/index.svelte
@@ -3,6 +3,11 @@
   $: question = scoped.question;
 </script>
 
+<svelte:head>
+  <script
+    src="https://www.wiris.net/demo/plugins/app/WIRISplugins.js?viewer=image"></script>
+</svelte:head>
+
 <article>
   {@html question.content}
 </article>
