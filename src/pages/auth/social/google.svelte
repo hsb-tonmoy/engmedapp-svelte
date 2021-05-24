c11448b0692f3a4e8b293e0cd95560b586549767
hsb-tonmoy <hsb.tonmoy@gmail.com>
2021-05-24 01:01:41 -0400

    Fixing Google login. v1

--- src/pages/auth/social/google.svelte
+++ src/pages/auth/social/google.svelte
@@ -3,11 +3,40 @@
 
   import { params } from "@roxi/routify";
 
-  import { socialLogin } from "../../../components/Auth/socialStore.js";
+  // import { socialLogin } from "../../../components/Auth/socialStore.js";
+
+  const API_URL = "https://api.engmedapp.com/";
 
   const provider = "google-oauth2";
 
   let code = $params.code;
+
+  async function socialLogin(provider, code) {
+    authenticating.set(true);
+    const res = await fetch(API_URL + "accounts/social/", {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        provider,
+        code,
+      }),
+    });
+    if (res.ok) {
+      const data = await res.json();
+      access_token.set(data.token);
+      await authenticate();
+    } else if (res.status === 400) {
+      loginError.set("Please try again.");
+      authenticating.set(false);
+      window.location.replace("/login");
+    } else {
+      console.log(res.status + res.statusText);
+      authenticating.set(false);
+    }
+  }
 
   onMount(() => {
     try {
@@ -15,7 +44,5 @@
     } catch (err) {
       console.log(err);
     }
-
-    console.log(provider + code);
   });
 </script>
